e91da2c93ef3ff8e295516819354b687a9876cda
szm1000 <48022212+szm1000@users.noreply.github.com>
2019-04-30 22:53:11 +0200

    Update Rational.h
    Alapvető operátorok implementálása, double értéket visszaadó függvény hozzáadása

--- Rational.h
+++ Rational.h
@@ -1,5 +1,6 @@
-#include <iostream>
+#include <iosfwd>
 #include <numeric>
+#include <stdexcept>
 
 class Rational{
 
@@ -10,25 +11,27 @@ class Rational{
 
     /*** Simplify to prevent overflow***/
 
-    void simplify(){
+    constexpr Rational& simplify(){
         int gcd = std::gcd(num, denom);
         if(gcd != 0){
             (*this).num /= gcd;
             (*this).denom /= gcd;
         }
+        return *this;
     };
+    
     public:
 
     /*** Constructors ***/
 
-    Rational(): num{0}, denom{1}{};
-    Rational(int a, int b){
-        (*this).num = a;
-        if(b != 0){
-            (*this).denom = b;
-        }
-        else{
-            std::cout<<"exception"<<std::endl;
+    constexpr Rational(int a): num{a}, denom{1}{}
+
+    //Defalut constructor
+    constexpr Rational(): Rational{0}{}
+
+    constexpr Rational(int a, int b): num{a}, denom{b}{
+        if(denom == 0){
+            throw std::domain_error{"Error: zero denominator"};
         }
         simplify();
     }
@@ -46,11 +49,37 @@ class Rational{
 
     /***Assignment operators***/
 
-    Rational& operator= (const Rational&);
-    Rational& operator+= (const Rational&);
-    Rational& operator-= (const Rational&);
-    Rational& operator*= (const Rational&);
-    Rational& operator/= (const Rational&);
+    constexpr Rational& operator=(const Rational& r){
+        num = r.num; 
+        denom = r.denom; 
+        return *this;
+    }
+
+    constexpr Rational& operator+=(const Rational& r){
+        num = num * r.denom + denom * r.num;
+        denom *= r.denom;
+        return simplify();
+    }
+
+    constexpr Rational& operator-=(const Rational& r){
+        num = num * r.denom - denom * r.num;
+        denom *= r.denom;
+        return simplify();
+    }
+
+    constexpr Rational& operator*=(const Rational& r){
+        num *= r.num;
+        denom *= r.denom;
+        return simplify();
+    }
+    constexpr Rational& operator/=(const Rational& r){
+        if(r.num == 0){
+            throw std::domain_error{"Error: division by zero"};
+        }
+        num *= r.denom;
+        denom *= r.num;
+        return simplify();
+    }
 
     /*** Other functions ***/
 
@@ -61,15 +90,97 @@ class Rational{
     int read_den() const& {
         return (*this).denom;
     }
+
+    double double_val() { 
+        return (*this).num/static_cast<double>((*this).denom);
+    }
 };
+
+Rational operator+(const Rational& r1, const Rational& r2){
+    Rational result = r1;
+    result += r2;
+    return result; 
+}
+
+Rational operator+(const Rational& r, const int & c){
+    Rational result(c);
+    result += r;
+    return result; 
+}
+
+Rational operator+(const int& c, const Rational& r){
+    Rational result(c);
+    result += r;
+    return result; 
+}
+
+Rational operator-(const Rational& r1, const Rational& r2){
+    Rational result = r1;
+    result -= r2;
+    return result; 
+}
+
+Rational operator-(const Rational& r, const int & c){
+    Rational result(-1*c);
+    result += r;
+    return result; 
+}
+
+Rational operator-(const int& c, const Rational& r){
+    Rational result(c);
+    result -= r;
+    return result; 
+}
+
+Rational operator*(const Rational& r1, const Rational& r2){
+    Rational result = r1;
+    result*= r2;
+    return result; 
+}
+
+Rational operator*(const Rational& r, const int& c){
+    Rational result(c);
+    result *= r;
+    return result; 
+}
+
+Rational operator*(const int& c, const Rational& r){
+    return r*c; 
+}
+
+Rational operator/(const Rational& r1, const Rational& r2){
+    Rational result = r1;
+    result/= r2;
+    return result; 
+}
+
+Rational operator/(const Rational& r, const int& c){
+    Rational result(r);
+    result /= Rational(c);
+    return result; 
+}
+
+Rational operator/(const int& c, const Rational& r){
+    Rational result{c};
+    result/= r;
+    return result; 
+}
 
 std::ostream& operator<<(std::ostream& o, Rational const& r){
     int d = r.read_den();
-    if (d == 1){
-        o<<r.read_num()<<" ";
+    int n = r.read_num();
+    if (d == 1 || n == 0){
+        o<<n<<" ";
     }
     else{
-        o<<r.read_num()<<"/"<<d<<" ";
+        o<<n<<"/"<<d<<" ";
     }
     return o;
+}
+
+std::istream& operator>>(std::istream& istr, Rational& r){
+    int a, b;
+    istr>>a>>b;
+    r = Rational{a, b};
+    return istr;
 }
